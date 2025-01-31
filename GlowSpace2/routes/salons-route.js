d63654bef20d703489dc93843e78f8246af3fb73
xfooxe <fffoo_o.12@hotmail.com>
2025-01-31 06:48:02 +0300

    Added new folder to the repository

---
+++ GlowSpace2/routes/salons-route.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const Salon = require('../models/salons');
+
+// Route to fetch all salons
+router.get('/', async (req, res) => {
+  try {
+    const salons = await Salon.find(); // Fetch all hotels from the database
+    res.render('salons', { salons, currentPath: req.path }); // Render thge salons.ejs view with the fetched data
+  } catch (error) {
+    console.error('Error fetching salons:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Route to show a single salon's information
+router.get('/:id', async (req, res) => {
+  try {
+    const salon = await Salon.findById(req.params.id);
+    console.log(req.path);
+    if (!salon) {
+      return res.status(404).send('Salon not found');
+    }
+    res.render('show-salon', { salon, currentPath: "salons"  });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;

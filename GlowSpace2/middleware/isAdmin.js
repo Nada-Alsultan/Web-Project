626f373462a8fa0963138129de5c6de657de3634
FAI <fffoo_o.12@hotmail.com>
2025-02-01 22:54:48 +0300

    Delete GlowSpace2 directory

--- GlowSpace2/middleware/isAdmin.js
+++
@@ -1,8 +0,0 @@
-function isAdmin(req, res, next) {
-    if (req.session.admin) {
-        next();  // Proceed if the session exists and belongs to an admin
-    } else {
-        res.redirect('/admin/login');
-    }
-}
-module.exports = isAdmin;
